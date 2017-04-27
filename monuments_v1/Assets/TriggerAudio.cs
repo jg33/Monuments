9b9f2b0af6728ccb22163764f0091ef0ee7c1e25
Jesse Garrison <jesse@takethefort.com>
2017-04-27 13:37:00 -0700

    audio control

---
+++ monuments_v1/Assets/TriggerAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour {
+
+	public AudioClip[] clips;
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	public void setCue(int _i){
+
+		if(clips[_i]){
+
+			gameObject.GetComponent<AudioSource>().PlayOneShot(clips[_i]);
+
+		}
+
+
+
+
+	}
+}
